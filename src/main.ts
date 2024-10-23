d78b27de9761a180d4d0453ee940aeccf4216d30
ArrayaWongsaita <Wongsaita.a@gmail.com>
2024-10-23 22:16:41 +0700

    install nestjs/config and config corsOrigin in main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './configs/port.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  const configService = app.get(ConfigService);
+  const corsOrigin = configService.get<string>('CORS_ORIGIN');
+
+  // Setting up CORS
+  app.enableCors({
+    origin: corsOrigin === '*' ? true : corsOrigin.split(','),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
